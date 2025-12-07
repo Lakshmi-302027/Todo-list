1eab7f564eceb1cea729875a2735289259ad1033
E Lakshmi <le9098967@gmail.com>
2025-12-07 10:18:58 +0530

    Add all files

---
+++ todo.js
@@ -0,0 +1,33 @@
+let todoList = [
+     {item:'Go to clg', 
+     dueDate:'2025-11-10'}
+];
+
+displayItems();
+
+function addTodo() {
+  let inputElement = document.querySelector('#todo-input');
+    let dateElement = document.querySelector('#todo-date');
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  todoList.push({item:todoItem, dueDate: todoDate});
+  dateElement.value = '';
+  inputElement.value = '';
+  displayItems();
+}
+
+function displayItems() {
+  let containerElement = document.querySelector('.todo-container');
+  
+  let newHtml = '';
+  for (let i = 0; i < todoList.length; i++) {
+    let {item, dueDate} = todoList[i];
+    newHtml += `
+      <span>${item}</span>
+      <span>${dueDate}</span>
+      <button class="btn-delete" onClick="todoList.splice(${i}, 1);displayItems();">Delete</button>
+    `;
+  }
+  containerElement.innerHTML = newHtml;
+}
+
